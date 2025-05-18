e28bb67fa04381a1adb9d0136379de6a98818b1a
CachoMX <30279963+CachoMX@users.noreply.github.com>
2025-05-18 00:33:59 -0500

    Update landing page with new sections and improved UI components

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   siteUrl: 'https://vixi.agency', // Replace with your actual domain
   generateRobotsTxt: true, // Generates robots.txt along with the sitemap
+  exclude: [
+    '/careers',
+    '/contact/thank-you'
+  ],
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,

9e0ad49a0dab3ba489988d40d69ecfebfee9f0a6
CachoMX <30279963+CachoMX@users.noreply.github.com>
2025-05-17 23:51:00 -0500

    feat: Text ratio increased

--- next.config.js
+++ next.config.js
@@ -2,23 +2,7 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: false
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/:any*',
-          has: [
-            {
-              type: 'host',
-              value: 'www.vixi.agency',
-            },
-          ],
-          destination: 'https://vixi.agency/:any*',
-        },
-      ],
-    }
-  },
+  }
 }
 
 module.exports = nextConfig
